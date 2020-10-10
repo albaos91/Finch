359eb2b4b25ff1e41d98a76eba0364daec917aff
Alba Osmanaj <albaos91@gmail.com>
2020-10-10 10:55:12 -0500

    pull

--- client/src/pages/Dashboard/dashboard.js
+++ client/src/pages/Dashboard/dashboard.js
@@ -5,7 +5,7 @@ import { logoutUser } from "../../actions/authActions";
 import TwitterAPI from "../../utils/TwitterAPI";
 import SavedTweets from "../../components/Saved/SavedTweets";
 import DashboardHome from "../../components/Dashboard/DashboardHome";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 
 class Dashboard extends Component {
   onLogoutClick = (e) => {
@@ -18,18 +18,9 @@ class Dashboard extends Component {
 
     return (
       <div>
-<<<<<<< HEAD
-        
-            
-            
-            <DashboardHome>
-                <Button onClick={this.onLogoutClick}>Logout</Button>
-            </DashboardHome> 
-=======
         <h3>You are sucessfully logged out.</h3>
         <button onClick={this.onLogoutClick}>Logout</button>
-          <DashboardHome />
->>>>>>> d29715776e08729a6970e9234dc5864844cc0000
+        <DashboardHome />
       </div>
     );
   }
